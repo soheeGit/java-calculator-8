388b3e4436a86a1875e4abacc27bdb97998b5698
soheeGit <63wlsthgml@gmail.com>
2025-10-16 20:43:57 +0900

    feat: 커스텀 구분자 파싱 기능

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,14 +1,26 @@
 package calculator;
 
 public class StringCalculator {
+    private static final String CUSTOM_DELIMITER_PREFIX = "//";
+    private static final String CUSTOM_DELIMITER_SUFFIX = "\\n";
+
     public static int calculate(String input) {
         if(input == null || input.isEmpty()) {
             return 0;
         }
 
         String delimiter = "[,:]";
+        String numbers = input;
 
-        return sum(input, delimiter);
+        if(input.startsWith(CUSTOM_DELIMITER_PREFIX)) {
+            int newlineIndex = input.indexOf(CUSTOM_DELIMITER_SUFFIX);
+            String customDelimiter = input.substring(2, newlineIndex);
+            numbers = input.substring(newlineIndex + CUSTOM_DELIMITER_SUFFIX.length());
+
+            delimiter = delimiter.substring(0, delimiter.length() - 1) + customDelimiter + "]";
+        }
+
+        return sum(numbers, delimiter);
     }
 
     private static int sum(String numbers, String delimiter) {
